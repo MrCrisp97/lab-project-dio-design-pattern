511bfce3d29d15161fb617bbc4dc2f3aff2a88b3
MrCrisp <cristianbpivoto15@gmail.com>
2024-06-23 13:45:25 -0300

    adicionado packages

--- src/main/java/com/service/Application.java
+++ src/main/java/com/service/Application.java
@@ -1,13 +1,13 @@
-package com.example.controleEstoque;
+package com.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ControleEstoqueApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControleEstoqueApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
